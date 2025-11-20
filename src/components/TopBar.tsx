a70a422aaf31899d5878bd803d76a53415da27e5
hariom-rcspl <hariom@rcspl.net>
2025-11-20 14:07:01 +0530

    profile dropdown added

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,17 +1,51 @@
-import { useLocation } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import TopGraph from "./TopGraph"
-
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "../components/ui/dropdown-menu"
+import { Button } from "./ui/button"
+import { User } from "lucide-react"
 
 const TopBar = () => {
     const { pathname } = useLocation()
     return (
         <div className="bg-black pb-24 -mb-10">
-            <header className="h-14 shadow-sm flex items-center px-5 text-white">
+            <header className=" py-3 shadow-sm flex items-center justify-between px-5 text-white">
                 <h2 className="font-semibold text-lg">Welcome</h2>
+                <div className="flex gap-3 items-center">
+                    <p className="text-sm font-bold">
+                        Hi, Vijay
+                    </p>
+                    <DropdownMenu modal={false}>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="outline" aria-label="Open menu" size="icon-sm">
+                                {/* <MoreHorizontalIcon /> */}
+                                <User color="black" />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent className="w-40" align="end">
+                            <DropdownMenuItem>
+                                <Link to={""}>Profile</Link>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <Link to={""}>Setting</Link>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <Link to={""}>Change Password</Link>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <Link to={""}>Logout</Link>
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
             </header>
 
             {
-                pathname === "/" &&  <TopGraph />
+                pathname === "/" && <TopGraph />
             }
         </div>
     )
